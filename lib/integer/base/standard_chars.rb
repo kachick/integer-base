c568757fdd130dbabf9ed2c4e169b0bb0af55784
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-05-18 08:21:10 +0900

    fix comments for a explaining data

--- lib/integer/base/standard_chars.rb
+++ lib/integer/base/standard_chars.rb
@@ -3,7 +3,7 @@ class Integer; module Base
   # @example
   #   { 1  => [:"0"],
   #     2  => [:"0", :"1"],
-  #     36 => [:"0", :"1", ... , :"35"] }
+  #     36 => [:"0", :"1", ... , :Z] }
   STANDARD_CHARS = Hash.new{|*|raise KeyError}.tap {|standards|
     1.upto 10 do |n|
       standards[n] = ('0'..((n - 1).to_s)).map(&:to_sym).freeze
