9d03a04923c1241492fe767950b2e31c9d98e2c0
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-05-18 09:30:16 +0900

    add modes when unary chars

--- lib/integer/base/eigen.rb
+++ lib/integer/base/eigen.rb
@@ -8,38 +8,117 @@ class Integer; module Base
     # @param [Array<#to_sym>] chars
     # @return [Integer]
     def parse(str, chars)
-      chars = base_chars_for chars
       str   = str.to_str.downcase
+
+      sign = parse_sign! str
+      abs = (
+        case chars.length
+        when 1
+          parse_unary_abs str, chars.first
+        else
+          parse_positional_abs str, chars
+        end
+      )
       
-      sign  = parse_sign! str
-      
-      case chars.length
-      when 1
-        parse_unary_abs str, chars.first
-      else
-        trim_paddings! str, chars.first
-        
-        abs = parse_abs str, chars
-        
-        (sign == :-) ? -abs : abs
-      end
-    end
-    
-    def parse_unary_abs(str, char)
-      chars = str.chars
-      p chars.to_a
-      atom = chars.to_a.uniq.first.to_sym
-      
-      raise InvalidCharacter, 'contain multiple chars' unless atom.length == 1
-      raise InvalidCharacter, 'not match the char and atom' unless atom == char
-      
-      chars.length
+      (sign == :-) ? -abs : abs
     end
 
     # @param [#to_int] num
     # @param [Array<#to_sym>] chars
     # @return [String]
     def convert_to_string(num, chars)
+      case chars.length
+      when 1
+        convert_to_string_unary num, chars.first
+      else
+        convert_to_string_positional num, chars
+      end
+    end
+
+    private
+
+    # @return [Symbol]
+    def parse_sign!(str)
+      sign = str.slice!(/\A([-+])/, 1)
+
+      if sign
+        if str.empty?
+          raise InvalidCharacter
+        else
+          sign.to_sym
+        end
+      else
+        :+
+      end
+    end
+
+    # @param [Array<#to_sym>] chars
+    # @return [Array<Symbol>]
+    def base_chars_for(chars)
+      chars = chars.map{|c|c.downcase.to_sym}
+
+      case
+      when chars.length < 2
+        raise TypeError, 'use 2 or more than characters'
+      when chars.dup.uniq!
+        raise InvalidCharacter, 'dupulicated characters'
+      when chars.any?{|s|s.length != 1}
+        raise InvalidCharacter, 'chars must be Array<Char> (Char: length 1)'
+      when chars.any?{|c|SPECIAL_CHAR =~ c}
+        raise InvalidCharacter, 'included Special Characters (-, +, space, control-char)'
+      else
+        chars
+      end
+    end
+
+    # @return [Symbol]
+    def unary_char_for(char)
+      raise InvalidCharacter unless (char.length == 1) and char.respond_to?(:to_sym)
+      
+      char.to_sym
+    end
+    
+    def trim_positional_paddings!(str, pad_char)
+      str.slice!(/\A#{pad_char}+/)
+    end
+    
+    # @return [Integer]
+    def parse_positional_abs(str, chars)
+      raise InvalidCharacter if str.empty?
+      base = chars.length
+      chars = base_chars_for chars
+      
+      str = str.dup
+      trim_positional_paddings! str, chars.first
+      
+      str.chars.reverse_each.with_index.inject(0) {|sum, char_index|
+        char, index = *char_index
+        
+        if radix = chars.index(char.to_sym)
+          sum + (radix * (base ** index))
+        else
+          raise InvalidCharacter
+        end
+      }
+    end
+    
+    # @return [Integer]
+    def parse_unary_abs(str, char)
+      char = unary_char_for char
+      return 0 if str.empty?
+      
+      chars = str.chars.to_a
+      atoms = chars.uniq      
+      raise InvalidCharacter, 'contain multiple chars' unless atoms.length == 1
+      
+      atom = atoms.first.to_sym
+      raise InvalidCharacter, 'not match the char and atom' unless atom == char
+      
+      chars.length
+    end
+  
+    # @return [String]
+    def convert_to_string_positional(num, chars)
       chars = base_chars_for chars
       int   = num.to_int
       base  = chars.length
@@ -57,53 +136,14 @@ class Integer; module Base
         s.insert 0, '-' if int < 0
       }
     end
-
-    def convert_unary(num, char)
-    end
-
-    private
-    
-    # @param [Array<#to_sym>] chars
-    # @return [Array<Symbol>]
-    def base_chars_for(chars)
-      chars = chars.map{|c|c.downcase.to_sym}
-
-      case
-      when chars.length < 1
-        raise TypeError, 'use 2 and more characters'
-      when chars.dup.uniq!
-        raise InvalidCharacter, 'dupulicated characters'
-      when chars.any?{|s|s.length != 1}
-        raise InvalidCharacter, 'chars must be Array<Char> (Char: length 1)'
-      when chars.any?{|c|SPECIAL_CHAR =~ c}
-        raise InvalidCharacter, 'included Special Characters (-, +, space, control-char)'
-      else
-        chars
-      end
-    end
-    
-    # @return [Symbol]
-    def parse_sign!(str)
-      sign = str.slice!(/\A([-+])/, 1)
-      sign ? sign.to_sym : :+
-    end
-    
-    def trim_paddings!(str, pad_char)
-      str.slice!(/\A#{pad_char}+/)
-    end
-    
-    # @return [Integer]
-    def parse_abs(str, chars)
-      base = chars.length
+  
+    # @return [String]
+    def convert_to_string_unary(num, char)
+      char = unary_char_for char
+      int = num.to_int
       
-      str.chars.reverse_each.with_index.inject(0) {|sum, char_index|
-        char, index = *char_index
-        
-        if radix = chars.index(char.to_sym)
-          sum + (radix * (base ** index))
-        else
-          raise InvalidCharacter
-        end
+      (char.to_s * int.abs).tap {|s|
+        s.insert 0, '-' if int < 0
       }
     end
 
--- test/test_integer-base.rb
+++ test/test_integer-base.rb
@@ -5,6 +5,14 @@ class TestIntegerBase < Test::Unit::TestCase
   def test_parse
     assert_equal(1, '0a'.to_i(['0', *'A'..'I']))
     
+    assert_equal(0, ''.to_i(['0']))
+    assert_equal(1, '0'.to_i(['0']))
+    assert_equal(2, '00'.to_i(['0']))
+
+    assert_raises Integer::Base::InvalidCharacter do
+      '01'.to_i(['0'])
+    end
+
     assert_raises Integer::Base::InvalidCharacter do
       '9a'.to_i(['0', *'A'..'I'])
     end
@@ -43,10 +51,10 @@ class TestIntegerBase < Test::Unit::TestCase
   
   def test_characterable
     assert_raises Integer::Base::InvalidCharacter do
-      '210'.to_i('1'..'2')
+      '210'.to_i(('1'..'2').to_a)
     end
     
-    assert_equal(21, '210'.to_i('0'..'2'))
+    assert_equal(21, '210'.to_i(('0'..'2').to_a))
   end
   
   def test_to_s
@@ -59,25 +67,52 @@ class TestIntegerBase < Test::Unit::TestCase
     assert_equal('101', 5.to_s(%w[0 1]))
     
     assert_equal('-aib', -192.to_s(['0', *'A'..'I']))
-    assert_equal('0', 0.to_s(%w[0 1]))
     assert_equal('-1', -1.to_s(%w[0 1]))
     assert_equal('-10', -2.to_s(%w[0 1]))
     assert_equal('-11', -3.to_s(%w[0 1]))
     assert_equal('-100', -4.to_s(%w[0 1]))
     assert_equal('-101', -5.to_s(%w[0 1]))
+    
+    assert_equal('0' * 192, 192.to_s(['0']))
+    assert_equal('', 0.to_s(['0']))
+    assert_equal('0', 1.to_s(['0']))
+    assert_equal('00', 2.to_s(['0']))
+    assert_equal('000', 3.to_s(['0']))
+
+    assert_equal("-#{'0' * 192}", -192.to_s(['0']))
+    assert_equal('-0', -1.to_s(['0']))
+    assert_equal('-00', -2.to_s(['0']))
+    assert_equal('-000', -3.to_s(['0']))
+
   end
   
   def test_binary
     assert_equal 2, Integer::Base.parse('10', %w[0 1])
   end
   
-  def test_standards
-    assert_equal(0, Integer::Base.parse('', Integer::Base::STANDARD_CHARS[1]))
-    
+  def test_standards_to_i
     assert_raises Integer::Base::InvalidCharacter do
       Integer::Base.parse('', Integer::Base::STANDARD_CHARS[2])
     end
     
+    assert_equal(0, Integer::Base.parse('', Integer::Base::STANDARD_CHARS[1]))
+    
+    assert_raises Integer::Base::InvalidCharacter do
+      Integer::Base.parse('-', Integer::Base::STANDARD_CHARS[2])
+    end
+    
+    assert_raises Integer::Base::InvalidCharacter do
+      Integer::Base.parse('-', Integer::Base::STANDARD_CHARS[1])
+    end
+
+    assert_raises Integer::Base::InvalidCharacter do
+      Integer::Base.parse('+', Integer::Base::STANDARD_CHARS[2])
+    end
+    
+    assert_raises Integer::Base::InvalidCharacter do
+      Integer::Base.parse('+', Integer::Base::STANDARD_CHARS[1])
+    end
+
     assert_equal(1, Integer::Base.parse('0', Integer::Base::STANDARD_CHARS[1]))
     assert_equal(2, Integer::Base.parse('00', Integer::Base::STANDARD_CHARS[1]))
     assert_equal('11'.to_i(2), Integer::Base.parse('11', Integer::Base::STANDARD_CHARS[2]))
